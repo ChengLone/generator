ba9fa9659d7d9339d65bc42a8eb83d56b93b3b77
ChengLone <2541403556@qq.com>
2021-10-10 17:50:35 +0800

    Mybatis-plus代码生成器（逆向工程）

---
+++ src/main/java/com/wang/seckill/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.wang.seckill.service;
+
+import com.wang.seckill.pojo.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author ChengLone
+ * @since 2021-10-10
+ */
+public interface IUserService extends IService<User> {
+
+}
